ec66bbd5c37543e42a45aac3b585cc3a9017c11e
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2022-02-22 02:19:17 +0100

    primera pantalla creada d'optometrista

--- optica/lib/app.dart
+++ optica/lib/app.dart
@@ -9,19 +9,99 @@ class App extends StatelessWidget {
     final user = FirebaseAuth.instance.currentUser;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-            )
-          ],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    "NomApp",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      _InputBox(name: "Nom"),
+                      SizedBox(height: 12),
+                      _InputBox(name: "Codi Deontologic"),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 0,
+                  child: _Button(name: "Accedeix"),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Center(
-          child: Text(user.email.toString()),
-        ),
+      ),
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  final name;
+  const _Button({
+    Key key,
+    this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(name),
+      style: ElevatedButton.styleFrom(
+          minimumSize: Size(200, 50),
+          shape: StadiumBorder(),
+          primary: Colors.grey),
+    );
+  }
+}
+
+class _InputBox extends StatelessWidget {
+  final name;
+  const _InputBox({
+    Key key,
+    this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 260,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(name),
+          Container(
+            height: 35,
+            child: TextField(
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
