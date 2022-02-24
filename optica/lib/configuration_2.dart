7da7deea97137a332c55a3d97173aeec32997d03
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2022-02-24 01:24:03 +0100

    trying to save and load data from firebase

--- optica/lib/configuration_2.dart
+++ optica/lib/configuration_2.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:optica/configuration_1.dart';
 import 'package:intl/intl.dart';
 import 'package:optica/principalApp.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'app.dart';
 
@@ -12,8 +14,29 @@ class Configuration_2 extends StatefulWidget {
   State<Configuration_2> createState() => _Configuration_2State();
 }
 
+final controller1_1 = TextEditingController();
+final controller1_2 = TextEditingController();
+final controller1_3 = TextEditingController();
+final controller1_4 = TextEditingController();
+final controller2_1 = TextEditingController();
+final controller2_2 = TextEditingController();
+final controller2_3 = TextEditingController();
+final controller2_4 = TextEditingController();
+
+List<TextEditingController> controllers = [
+  controller1_1,
+  controller1_2,
+  controller1_3,
+  controller1_4,
+  controller2_1,
+  controller2_2,
+  controller2_3,
+  controller2_4
+];
+
 class _Configuration_2State extends State<Configuration_2> {
   DateTime _dateTime;
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -74,9 +97,7 @@ class _Configuration_2State extends State<Configuration_2> {
                           shape: const StadiumBorder(),
                           primary: Colors.grey,
                           onPrimary: Colors.black),
-                      onPressed: () {
-                        //guardar a cloud
-                      },
+                      onPressed: () {},
                     ),
                   ],
                 )),
@@ -93,7 +114,45 @@ class _Configuration_2State extends State<Configuration_2> {
                   onPressed: () {
                     showDialog(
                         context: context,
-                        builder: (BuildContext context) => _PopUp(context));
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Historial'),
+                            content: StreamBuilder(
+                              stream: db
+                                  .collection("usuarios")
+                                  .doc(FirebaseAuth.instance.currentUser.email
+                                      .toString())
+                                  .collection("historial")
+                                  .snapshots(),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                                if (snapshot.hasError) {
+                                  return const CircularProgressIndicator();
+                                } else if (snapshot.hasData) {
+                                  return const Text("Hi ha data!"); //error
+                                  // return ListView(
+                                  //     children: snapshot.data.docs.map((e) {
+                                  //   return Card(
+                                  //     child: ListTile(
+                                  //       title: Text(e['tipo']),
+                                  //     ),
+                                  //   );
+                                  // }).toList());
+                                } else {
+                                  return Container();
+                                }
+                              },
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Tanca'),
+                              ),
+                            ],
+                          );
+                        });
                   },
                 ),
               ),
@@ -123,41 +182,78 @@ class _Configuration_2State extends State<Configuration_2> {
   }
 }
 
-Widget _PopUp(BuildContext context) {
-  return AlertDialog(
-    title: const Text('Popup example'),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-      TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: const Text('Tanca'),
-      ),
-    ],
-  );
-}
-
 class _LentInfo extends StatelessWidget {
-  const _LentInfo({Key key}) : super(key: key);
+  final numRow;
+  const _LentInfo({Key key, this.numRow}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-          child: Icon(Icons.remove_red_eye_outlined, size: 80),
+          child: const Icon(Icons.remove_red_eye_outlined, size: 80),
         ),
-        MainInput(),
-        MainInput(),
-        MainInput(),
-        MainInput(),
+        const SizedBox(
+          width: 120,
+          height: 65,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: controllers[1], //error
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "dubte",
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 120,
+          height: 65,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "dubte",
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 120,
+          height: 65,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "dubte",
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 120,
+          height: 65,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "dubte",
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
