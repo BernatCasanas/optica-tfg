957f81e9b01e1aa9f538d89b3bd6662a8d789e9a
Pau Fernández <pau.fernandez@upc.edu>
2022-04-23 17:47:35 +0200

    Arreglo imports

--- optica/lib/main.dart
+++ optica/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'auth_gate.dart';
-import 'app.dart';
-import 'firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:optica/app.dart';
+import 'package:optica/auth_gate.dart';
+import 'package:optica/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
