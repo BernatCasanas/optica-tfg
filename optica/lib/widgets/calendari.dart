3c14d2094a90868b4d860e6bbab2463faef10b22
Pau Fernández <pau.fernandez@upc.edu>
2022-06-01 12:14:25 +0200

    Arreglo calendari
    - El bug era problema de UTC... ;)

--- optica/lib/widgets/calendari.dart
+++ optica/lib/widgets/calendari.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:optica/model/user.dart';
 import 'package:optica/utils/event.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,94 +12,111 @@ class Calendari extends StatefulWidget {
 }
 
 class _CalendariState extends State<Calendari> {
-  DateTime selectedDay1 = DateTime.now();
+  DateTime _selectedDay1 = DateTime.now();
   DateTime _focusedDay = DateTime.now();
-  Map<DateTime, List<Event>> selectedEvents = {};
   CalendarFormat _calendarFormat = CalendarFormat.month;
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  List<Event> _getEventsfromDay(DateTime date) {
-    return selectedEvents[date] ?? [];
+  Map<DateTime, List<Event>> sortEvents(eventList) {
+    Map<DateTime, List<Event>> eventMap = {};
+    for (final event in eventList) {
+      final DateTime tiempo = event["tiempo"].toDate();
+      final nombre = event['nombre'];
+      final date = DateTime.utc(tiempo.year, tiempo.month, tiempo.day);
+      eventMap.putIfAbsent(date, () => []);
+      eventMap[date]!.add(Event(title: nombre));
+    }
+    return eventMap;
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection("usuarios")
-            .doc(FirebaseAuth.instance.currentUser?.email)
-            .collection("avisos")
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            selectedEvents.clear();
-            snapshot.data?.docs.forEach(
-              (doc) {
-                DateTime date = doc["tiempo"].toDate();
-                var nombre = doc['nombre'];
-                date = DateTime(date.year, date.month, date.day);
-                if (selectedEvents[date] != null) {
-                  selectedEvents[date]?.add(Event(title: nombre));
-                } else {
-                  selectedEvents[date] = [Event(title: nombre)];
-                }
-              },
-            );
+      stream: FirebaseFirestore.instance
+          .collection("usuarios")
+          .doc(FirebaseAuth.instance.currentUser?.email)
+          .collection("avisos")
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          final eventMap = sortEvents(snapshot.data!.docs);
+          debugPrint(eventMap.toString());
+          return Column(
+            children: [
+              TableCalendar<Event>(
+                locale: "es_ES",
+                headerStyle: const HeaderStyle(formatButtonVisible: false),
+                firstDay: DateTime.utc(2022, 1, 1),
+                lastDay: DateTime.utc(2030, 12, 31),
+                focusedDay: _focusedDay,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay1, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay1 = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                calendarFormat: _calendarFormat,
+                onFormatChanged: (format) {
+                  setState(() => _calendarFormat = format);
+                },
+                eventLoader: (DateTime date) {
+                  debugPrint(date.toString());
+                  return eventMap[date] ?? [];
+                },
+              ),
+              Expanded(
+                child: _EventsForDay(
+                  events: eventMap[_selectedDay1] ?? [],
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
 
-            return Column(
-              children: [
-                TableCalendar(
-                  locale: "es_ES",
-                  headerStyle: const HeaderStyle(formatButtonVisible: false),
-                  firstDay: DateTime.utc(2022, 1, 1),
-                  lastDay: DateTime.utc(2030, 12, 31),
-                  focusedDay: _focusedDay,
-                  selectedDayPredicate: (day) {
-                    return isSameDay(selectedDay1, day);
-                  },
-                  onDaySelected: (selectedDay, focusedDay) {
-                    setState(() {
-                      selectedDay1 = selectedDay;
-                      _focusedDay = focusedDay;
-                    });
-                  },
-                  calendarFormat: _calendarFormat,
-                  onFormatChanged: (format) {
-                    setState(() {
-                      _calendarFormat = format;
-                    });
-                  },
-                  eventLoader: _getEventsfromDay,
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          SizedBox(height: 10),
-                          ..._getEventsfromDay(selectedDay1)
-                              .map((e) => ListTile(
-                                    title: Text(e.title),
-                                  )),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        });
+class _EventsForDay extends StatelessWidget {
+  const _EventsForDay({
+    Key? key,
+    required this.events,
+  }) : super(key: key);
+
+  final List<Event> events;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "Events",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ...events.map(
+              (e) => ListTile(
+                title: Text(e.title),
+                tileColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
